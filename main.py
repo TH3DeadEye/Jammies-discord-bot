01d9e3a655bfa77734158a89836ae63e7db63842
TH3DeadEye <armanmilani84@gmail.com>
2022-04-09 18:58:56 +0430

    init

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-#load libraries
-import os
 import discord
-from discord.ext.commands import Bot
+import os
+# load our local env so we dont have the token in public
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
 load_dotenv()
-
-bot = Bot("!")
-
+bot = commands.Bot(command_prefix='?')  # prefix our commands with '.'
+#this event will be called when the bot is ready to use 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    bot.change_presence(activity=discord.Activity(name ="?Help", type = discord.ActivityType.watching))
+    print(f"{bot.user.name} - {bot.user.id}")
+    print('Jammies is ready!')
 
+
+#setting up the commands
 @bot.command()
-async def  ping(ctx):
-    await ctx.send("pong")
-    print(ctx.message.author.name + " has pinged the bot")
+async def clear(ctx , amount = 100):
+    channel = ctx.message.channel
+    messages = []
+
+    async for message in channel.history(limit=amount):
+        messages.append(message)
+
+    await channel.delete_messages(messages)
+    await ctx.send("Messages have been cleared")
+
 
 bot.run(os.getenv('TOKEN'))
